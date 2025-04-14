ed6f08e54a8186385f5bcbc57f58a47aab5ca9fd
MarcoRoma96 <mar.roma96@gmail.com>
2025-04-14 22:36:05 +0200

    updated first 5 days description

--- skogabot.py
+++ skogabot.py
@@ -21,48 +21,104 @@ logger = logging.getLogger(__name__)
 # Itinerario statico: messaggi definiti a mano (hard-coded)
 STATIC_ITINERARY = {
     "1": (
-        "**Giorno 1 (19/04/2025):**\n"
-        "Arrivo a Keflavik, recupero bagagli e noleggio auto. Trasferimento al Downtown Reykjavík Apartments, "
+        "*Giorno 1 (19/04/2025):*\n"
+        "(19:45 circa) Arrivo a Keflavik, recupero bagagli e noleggio auto. Trasferimento al Downtown Reykjavík Apartments, "
         "cena e visita opzionale a Reykjavík."
+        "Link utili della giornata:\n"
+        "Rental auto: [Blue Car Rental](https://www.bluecarrental.is/)\n"
+        "Mappa per raggiungere il noleggio auto a piedi: (https://www.bluecarrental.is/useful-information/deliveries/office-pick-up/)"
+        "Aeroporto - Alloggio (https://maps.app.goo.gl/cThxLUWXpugVaxvS9)"
+        "ALLOGGIO: Downtown Reykjavík Apartments - 2 bagni (https://www.booking.com/Share-9J3Xdfu)"
     ),
     "2": (
-        "**Giorno 2 (20/04/2025):**\n"
-        "Colazione e spesa al supermercato. Partenza per Þingvellir, visita al Parco Nazionale, poi attrazioni come "
-        "Haukadalur, il geyser Strokkur, la cascata Gullfoss e Seljalandsfoss. Fine giornata con trasferimento "
-        "alla Countryhouse e cena a Hvolsvöllur."
+        "* Giorno 2 (20/04/2025): * \n"
+        "Colazione (8:30) e spesa al supermercato (9:00 - 10:00). Partenza per Þingvellir, visita al Parco Nazionale, poi attrazioni come "
+        "Haukadalur (famosa vallata con diversi geyser geotermici antichi, sorgenti termali e pozze di fango ribollenti.), "
+        "il geyser Strokkur (geyser geotermico molto visitato che erutta ogni 8-10 minuti e raggiunge altezze di 20 metri), "
+        "Sorgenti termali di Geysir: famoso geyser che erutta ogni pochi minuti e spara acqua fino a 70 metri in aria.\n"
+        "la cascata Gullfoss, cascata iconica, nota per il suo salto a più gradini lungo una curva di 90° del fiume Hvitá, "
+        "e Seljalandsfoss, dove i visitatori possono passeggiare dietro questa cascata alta 60 metri, alimentata da un ghiacciaio vulcanico, che precipita da una parete scoscesa."
+        "\n Fine giornata con trasferimento alla Countryhouse e cena volendo a Hvolsvöllur, paesino vicino da visitare."
+        "Link utili della giornata:\n"
+        "Reykjavík - Þingvellir (https://maps.app.goo.gl/bAgQwxKHeedWdTey7) (1h) \n I parcheggi sono a pagamento (500 - 1000 ISK). Il parcheggio P1 ha i servizi igienici"
+        "Þingvellir - Haukadalur (https://maps.app.goo.gl/Wn5fdbsN12uCbJ1f)"
+        "Strokkur - Gullfoss (https://maps.app.goo.gl/y3GM5UQwJDXW7NPm7) (15 min)"
+        "Gullfoss - Seljalandsfoss (https://maps.app.goo.gl/XRwmiWGngp7Xc2fj9) (1h e 45 min)"
+        "ALLOGGIO: Countryhouse with great view on Eyjafjallajökull - 1 bagno (https://www.booking.com/Share-EeRs87)"
     ),
     "3": (
-        "**Giorno 3 (21/04/2025):**\n"
-        "Partenza mattutina per Butra, visita a Skógafoss (con pranzo al sacco), Sólheimajökull e Solheimasandur. "
-        "Proseguimento verso Dyrhólaey Beach con pernottamento all’Hotel Katla e cena in albergo."
+        "* Giorno 3 (21/04/2025): * \n"
+        "Partenza mattutina (9:30) per Butra, visita a Skógafoss (con pranzo al sacco): suggestive cascate alte 60 metri da una parete rocciosa a strapiombo lungo l'antica linea costiera, dotate di piattaforma panoramica.\n"
+        "* Riccardo Consiglia: * lunghezza massima del sentiero porta fino a Skalabrekkufoss passando per altre 2 cascate, andata e ritorno sono sue 7 km, circa 2h15 minuti."
+        "Prosegue la giornata con, Sólheimajökull, questo grande ghiacciaio offre escursioni guidate molto apprezzate, in un ambiente sereno e panoramico."
+        "Senza escursione, solo per raggiungere il ghiacciaio ci vogliono 20 minuti dal parcheggio (40 minuti a/r)."
+        "Raggiungiamo quindi Solheimasandur: resti di un aereo della Marina degli Stati Uniti precipitato nel 1973 senza vittime, abbandonati su una spiaggia di sabbia nera."
+        "* Riccardo Consiglia: * da fare a piedi e dista quasi 4km dal parcheggio, da fare in piena spiaggia lavica. Sono circa 40 minuti andare e 40 tornare. Serviranno all’incirca 2 ore a piedi."
+        "C’è un bus in alternativa ma dicono che si paga 15-20€\n"
+        "Proseguimento verso Dyrhólaey Beach, la spiaggia nera, Promontorio Dyrhólaey. \n Pernottamento all’Hotel Katla e cena in albergo."
+        "Link utili della giornata:\n"
+        "Butra - Skógafoss (https://maps.app.goo.gl/iaN5ewtDpuStuUhaA) (1h)"
+        "Skógafoss - Sólheimajökull (https://maps.app.goo.gl/NuK61z9C8se4gtTS6) (30 min)"
+        "Sólheimajökull - Solheimasandur (30 min)"
+        "Solheimasandur - Dyrhólaey Beach (40 min)"
+        "Dyrhólaey Beach: spiaggia nera (https://maps.app.goo.gl/oZww5kwPNooD19qMA)"
+        "ALLOGGIO: Hotel Katla by Keahotels - 3 bagni (https://www.booking.com/Share-HfF8z5)"
     ),
     "4": (
-        "**Giorno 4 (22/04/2024):**\n"
-        "Partenza presto per il canyon Fjaðrárgljúfur, seguito dal pranzo. Tour del ghiacciaio Vatnajökull, visita a "
-        "Diamond beach e pernottamento presso Höfn Inn Guesthouse. Cena inclusa."
+        "* Giorno 4 (22/04/2024): * \n"
+        "ORE 8:30!!!!!!!     Dai, veloci a togliere la neve, si parte!\n"
+        "Partenza presto per il canyon Fjaðrárgljúfur, Fjaðrárgljúfur: profondo e tortuoso canyon fluviale, risalente a circa 2 milioni di anni fa, con percorso pedonale e viste panoramiche."
+        "Dal parcheggio al punto panoramico sono 20 minuti a piedi. \nSegue pranzo. \nTour del ghiacciaio Vatnajökull!"
+        "- Se, e dico SOLO sei, dovesse restare del tempo e delle forze in noi, si può tornare un po' indietro e vedere "
+        "Svartifoss (Skaftafell): una cascata sottile, alta 20 metri, che precipita al centro di una spettacolare parete tridimensionale di colonne esagonali di basalto."
+        "Il percorso dal parcheggio alla cascata sono 30/40 minuti a piedi -"
+        "D'obbligo è invece Diamond beach suggestiva spiaggia di sabbia nera, famosa per gli enormi e luccicanti frammenti di iceberg che finiscono sulla riva."
+        "Pernottamento presso Höfn Inn Guesthouse. Cena inclusa."
+        "Link utili della giornata:\n"
+        "Tour del ghiacciaio Vatnajökull"
+        "dove ? a che ora ? durata ? costo ?"
+        "https://drive.google.com/file/d/1Xh2YAj33JVGERJqaZ47xXH_C1ZQTXlsj/view?usp=drive_link (biglietti)"
+        "https://maps.app.goo.gl/SUgBxyny23CFSMax9 (luogo incontro) "
+        "[Link al tour] (https://troll.is/ice-caves-in-iceland/ice-cave-tours/crystal-blue-ice-cave/?_gl=1*1b6x4kl*_up*MQ..*_gs*MQ..&gclid=CjwKCAiAyJS7BhBiEiwAyS9uNU3gcTtzSi2U0K60W4xzRLxtnJZHLnEtCtj7xIXyzRKhQ_c-XqckkhoC890QAvD_BwE) 150€"
+        "\nALLOGGIO: Höfn Inn Guesthouse - 3 bagni https://www.booking.com/Share-UCSfQO"
     ),
     "5": (
-        "**Giorno 5 (23/04/2025):**\n"
-        "Lungo viaggio in auto con tappe a Hofn e visita al Stuðlagil Canyon, Dettifoss, Hverir e ai Mývatn "
-        "Nature Baths. Pernottamento al Fosshótel Mývatn, cena al sacco."
+        "* Giorno 5 (23/04/2025): * \n"
+        "Sveglia sveglia Skogabimbetti! sono le 8:30 ed è ora di partire, tanta strada Islandese all'orizzonte! "
+        "Il lungo viaggio di oggi prevede dopo un lungo viaggio in macchina tappe a Hofn con visita al Stuðlagil Canyon: "
+        "sentieri, panorami e cascate (Stuðlafoss) attorno a colonne di basalto, con avvistamenti primaverili di oche zamperose che nidificano."
+        "Pausetta per il pranzo al sacco, e poi via! fino a Dettifoss, la cascata più potente d'Europa, apparsa nel film 'Prometheus', si trova nel Parco nazionale di Vatnajökull."
+        "Si raggiunge dal parcheggio in 15-20 minuti.\n"
+        "_Riccardo consiglia:_ Per il lato est prendere la F864. Per il lato ovest prendere la strada 862 per arrivare sul lato ovest. Lato est è più bello se si vuole vedere anche Selfoss, "
+        "mentre arrivi di lato a Dettifoss. Lato ovest vedi Dettifoss da davanti ma vedi meno Selfoss. Solitamente viene suggerito il lato est, meno schizzi e si vedono entrambe le cascate."
+        "Ci spostiamo in serata a Hverir, luogo geotermico noto per le sue pozze di fango gorgoglianti e le fumarole fumanti che emettono gas solforico (= puzza di marcio). C’è anche una grotta con dentro un laghetto in zona: Grjótagjá"
+        "Per concludere con un po' di relax, e soprattutto ghiaccione al pipo, per chi ce l'ha, e a tutto il resto ai restanti, tappa a Mývatn Nature Baths, terme a 52 euro a persona.\n"
+        "_Riccardo consiglia:_ Porta accappatoio, salvietta e ciabatte per non pagare il supplemento e ovviamente il costume da bagno."
+        "Pernottamento al Fosshótel Mývatn, cena al sacco."
+        "Link e info utili della giornata:\n"
+        "Hofn - Stuðlagil Canyon (240km-310km a seconda della strada) (3h e 45 min)"
+        "Stuðlagil Canyon - Dettifoss (lato ovest) (https://maps.app.goo.gl/GbGuA2fqbWFskfwE7) (1h e 40 min)"
+        "Dettifoss - Hverir (40 min di strada)"
+        "10 minuti per le terme"
+        "ALLOGGIO: Fosshótel Mývatn - 4 bagni (https://www.booking.com/Share-1fNvUWs)"
     ),
     "6": (
-        "**Giorno 6 (24/04/2025):**\n"
+        "* Giorno 6 (24/04/2025): * \n"
         "Sumardagurinn fyrsti: viaggio in auto e tappe a Goðafoss, Reykjavjafoss, Víðimýri e Berserkjahraun con "
         "eventuale visita al museo dello squalo. Pernottamento presso Snæfellsjökull Apartments."
     ),
     "7": (
-        "**Giorno 7 (25/04/2025):**\n"
+        "* Giorno 7 (25/04/2025): * \n"
         "Tour locale con visite a Faro Svortuloft, Saxholl crater, Holaholar, Djupalonssandur, Londrangar, "
         "Arnarstapi, Budakirkja, Ytri Tunga e faro di Akranes. Pernottamento al Moar guesthouse."
     ),
     "8": (
-        "**Giorno 8 (26/04/2025):**\n"
+        "* Giorno 8 (26/04/2025): * \n"
         "Partenza per Fagradalsfjall. Visita a terme Hvammsvik, alla cascata Glymur e al relitto Beached Whalers "
         "vicino a Reykjavík. Pernottamento a Guesthouse Maximilian a Keflavik."
     ),
     "9": (
-        "**Giorno 9 (27/04/2025):**\n"
+        "* Giorno 9 (27/04/2025): * \n"
         "Happy Birthday!🐋"
         "Ultima giornata in Islanda: consegna dell'auto, auguri di compleanno, imbarco bagagli e partenza per il volo di ritorno a Milano Berlusconi."
     )
@@ -198,7 +254,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         "Ciao! Sono il bot del viaggio in Islanda 😎.\n\n"
         "Per conoscere tutte le funzionalità, usa il comando /help."
     )
-    await update.message.reply_text(welcome_text)
+    await update.message.reply_text(welcome_text, parse_mode='markdown')
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
@@ -213,9 +269,9 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
         "/weather <città>    - Mostra le previsioni meteo per la città richiesta (default: Reykjavik).\n"
         "/volcano            - Controlla lo stato vulcanico attuale.\n"
         "/curiosita          - Ricevi una curiosità divertente del giorno."
-        "/subscribe_recipe          - Iscriviti per ricevere automaticamente le ricette del giorno (dal 19/04 al 27/04).\n"
+        "/subscribe_recipe   - Iscriviti per ricevere automaticamente le ricette del giorno (dal 19/04 al 27/04).\n"
     )
-    await update.message.reply_text(help_text)
+    await update.message.reply_text(help_text, parse_mode='markdown')
 
 async def piano(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
@@ -233,7 +289,7 @@ async def piano(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     else:
         reply_text = f"Ops, non ho trovato il piano per il Giorno {day_requested}. Controlla il numero e riprova!"
     
-    await update.message.reply_text(reply_text)
+    await update.message.reply_text(reply_text, parse_mode='markdown')
 
 async def weather(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
@@ -246,14 +302,14 @@ async def weather(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     forecast = get_weather_forecast(city=location)
     print(f"Forecast: {forecast}")
     print(type(update.message))
-    await update.message.reply_text(forecast)
+    await update.message.reply_text(forecast, parse_mode='markdown')
 
 async def volcano(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
     Comando /volcano: restituisce lo stato vulcanico attuale.
     """
     status = get_volcano_status()
-    await update.message.reply_text(status)
+    await update.message.reply_text(status, parse_mode='markdown')
 
 async def cacca(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
@@ -273,7 +329,7 @@ async def cacca(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     except ValueError as ve:
         status = "Stai cercando di fare dei danni 😡"
 
-    await update.message.reply_text(status)
+    await update.message.reply_text(status, parse_mode='markdown')
 
 async def curiosita(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
@@ -292,7 +348,7 @@ async def curiosita(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         "📖 Il Natale è magico e un po’ strano. Invece di Babbo Natale, ci sono 13 Jólasveinar (gli “Yule Lads”), ognuno con un comportamento bizzarro — come rubare cibo o spiare i bambini. Compaiono uno alla volta, dal 12 dicembre fino a Natale."
     ]
     reply = random.choice(curiosities)
-    await update.message.reply_text(f"🌋 Curiosità del giorno: {reply}")
+    await update.message.reply_text(f"🌋 Curiosità del giorno: {reply}", parse_mode='markdown')
 
 
 
@@ -343,7 +399,7 @@ async def scheduled_recipe(context: ContextTypes.DEFAULT_TYPE):
     print(f"chat: {chat_id}\n")
     recipe_text = RECIPES[RECIPE_INDEX]
     RECIPE_INDEX += 1
-    await context.bot.send_message(chat_id=chat_id, text=f"🍽️ **Ricetta del momento:**\n\n{recipe_text}")
+    await context.bot.send_message(chat_id=chat_id, text=f"🍽️ *Ricetta del momento:*\n\n{recipe_text}")
 
 async def subscribe_recipe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
